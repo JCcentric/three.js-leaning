2c64eedff7f4e7505c7e97f232effde2d6e04dd6
Jared Gilbert <jgilbert6867@gmail.com>
2025-11-29 18:33:56 -0500

    Saving progress

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ const scene = new THREE.Scene();
 
 //Perspective camera(FOV in degrees, aspect ration width of element/height, near clipping pane, far clipping plane which helps with performance)
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 50;
 
 //Renderer setup  
 const renderer = new THREE.WebGLRenderer();
@@ -21,6 +22,7 @@ If you wish to keep the size of your app but render it at a lower resolution, yo
 Last but not least, we add the renderer element to our HTML document. This is a <canvas> element the renderer uses to display the scene to us.
 */
 renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setClearColor(0xffffff);
 document.body.appendChild(renderer.domElement);
 
 
@@ -35,18 +37,20 @@ The third thing we need is a Mesh. A mesh is an object that takes a geometry, an
 
 By default, when we call scene.add(), the thing we add will be added to the coordinates (0,0,0). This would cause both the camera and the cube to be inside each other. To avoid this, we simply move the camera out a bit by adding z: 5 to its position.
 */
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const geometry = new THREE.TorusKnotGeometry( 10, 3, 100, 16 );
+const material = new THREE.MeshDepthMaterial();
+const torus = new THREE.Mesh( geometry, material );
 
-camera.position.z = 5;
+
+scene.add( torus );
+
+
 
 
 //Render the scene
 function animate() {
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    torus.rotation.y += 0.01;
+    torus.rotation.x += 0.01;
     renderer.render( scene, camera );
 }
 
